dda8c6bce751b8d2a2848d67eadd9cfef5d7b292
Rafał Sojecki <s26286@pjwstk.edu.pl>
2024-11-22 23:18:51 +0100

    Add position checking, movement, printing board

--- 03/game.ts
+++ 03/game.ts
@@ -11,74 +11,202 @@ export type Grid = Tile[][]
 
 export function getEgdeCoordinates(grid: Grid): Position[] {
     const edgeCoordinates: Position[] = [];
-    const rows = grid[0]?.length || 0;
-    const cols = grid.length;
+    const cols = grid[0]?.length || 0;
+    const rows = grid.length;
 
     if (rows === 0 || cols === 0) {
         return [];
     }
 
     // Left
-    for (let y = 0; y < cols; y++) {
+    for (let y = 1; y < rows - 1; y++) {
         edgeCoordinates.push({ x: 0, y });
     }
 
     // Right
-    for (let y = 0; y < cols; y++) {
-        edgeCoordinates.push({ x: rows - 1, y });
+    for (let y = 1; y < rows - 1; y++) {
+        edgeCoordinates.push({ x: cols - 1, y });
     }
 
-    // Top (excluding corners)
-    for (let x = 1; x < rows - 1; x++) {
+    // Top
+    for (let x = 0; x < cols; x++) {
         edgeCoordinates.push({ x, y: 0 });
     }
 
-    // Bottom (excluding corners)
-    for (let x = 1; x < rows - 1; x++) {
-        edgeCoordinates.push({ x, y: cols - 1 });
+    // Bottom
+    for (let x = 0; x < cols; x++) {
+        edgeCoordinates.push({ x, y: rows - 1 });
     }
 
     return edgeCoordinates;
 }
 
-export function left(): boolean {
-    return true
+export function checkMove(grid: Grid, futurePosition:Position ): Position | boolean {
+    if (!isValidPosition(grid, futurePosition)) {
+        return false
+    }
+    const futureField = getFieldGrid(grid, futurePosition);
+    if (futureField === 'empty' || futureField === 'wall' || futureField === 'start') {
+        return futurePosition
+    }
+
+    if (futureField === 'end') {
+        return true
+    }
+    return false
 }
 
-export function right(): boolean {
-    return true
+export function left(grid: Grid, currentPosition: Position): Position | boolean {
+    const futurePosition: Position = {x:currentPosition.x - 1, y:currentPosition.y} 
+    return checkMove(grid, futurePosition)
 }
 
-export function top(): boolean {
-    return true
+export function right(grid: Grid, currentPosition: Position): Position | boolean {
+    const futurePosition: Position = {x:currentPosition.x + 1, y:currentPosition.y} 
+    return checkMove(grid, futurePosition)
 }
 
-export function bottom(): boolean {
-    return true
+export function up(grid: Grid, currentPosition: Position): Position | boolean {
+    const futurePosition: Position = {x:currentPosition.x, y:currentPosition.y - 1} 
+    return checkMove(grid, futurePosition)
 }
 
-export function checkMove(): boolean {
-    return true
+export function down(grid: Grid, currentPosition: Position): Position | boolean {
+    const futurePosition: Position = {x:currentPosition.x, y:currentPosition.y + 1} 
+    return checkMove(grid, futurePosition)
 }
 
-export function isMoveOutBoard(): boolean {
-    return true
-}
-
-export function isMoveInObstacle(): boolean {
-    return true
-}
-
-export function generateBoard(cols: number, rows: number, obstacleCount: number): Grid {
+export function generateBoard(cols: number, rows: number): Grid {
     const grid: Grid = [];
 
     if (rows < 5 || cols < 5) {
         throw new Error("Board to small")
     }
 
-    for (let x = 0; x < rows; x++) {
-        const col: Tile[] = new Array<Tile>(cols).fill('empty');
-        grid.push(col)
+    if (rows < 0 || cols < 0) {
+        throw new Error("Negative values")
+    }
+
+    for (let y = 0; y < rows; y++) {
+        const row: Tile[] = new Array<Tile>(cols).fill('empty');
+        grid.push(row)
     }
     return grid
 }
+
+export function getFieldGrid(grid: Grid, position: Position) {
+    const { x, y } = position;
+    return grid[y][x];
+}
+
+export function addFieldGrid(grid: Grid, position: Position, tile: Tile) {
+    const { x, y } = position;
+    const cols = grid[0]?.length || 0;
+    const rows = grid.length;
+    if (rows < y || cols < x || y < 0 || x < 0) {
+        throw new Error("Outside range of board")
+    }
+    if (grid[y][x] === "start" || grid[y][x] === "end") {
+        throw new Error("Start/end field covered")
+    }
+    grid[y][x] = tile
+}
+
+export function isValidPosition (grid: Grid, pos: Position):boolean {
+    const rows = grid.length;
+    const cols = grid[0]?.length || 0;
+    return pos.x >= 0 && pos.x < cols && pos.y >= 0 && pos.y < rows;
+}
+    
+
+export function isNextToOrTheSamePosition(grid: Grid, a: Position, b: Position): boolean {
+
+    const isTheSamePosition = () => a.x === b.x && a.y === b.y
+
+    if (!isValidPosition(grid, a) || !isValidPosition(grid, b)) {
+        throw new Error("Outside range of board");
+    }
+
+    if (isTheSamePosition()) {
+        return true
+    }
+
+    const dx = Math.abs(a.x - b.x);
+    const dy = Math.abs(a.y - b.y);
+
+    return (dx === 1 && dy === 0) || (dx === 0 && dy === 1);
+}
+
+export function generateObstacles(grid: Grid, numObstacles: number): void {
+    const rows = grid.length;
+    const cols = grid[0]?.length || 0;
+
+    const maxObstacles = rows * cols / 4;
+
+    if (numObstacles > maxObstacles) {
+        throw new Error("Too many obstacles")
+    }
+
+    let placedObstacles = 0;
+
+    while (placedObstacles < numObstacles) {
+        const randomRow = Math.floor(Math.random() * rows);
+        const randomCol = Math.floor(Math.random() * cols);
+
+        if (grid[randomRow][randomCol] === "empty") {
+            addFieldGrid(grid, { x: randomCol, y: randomRow }, "obstacle")
+            placedObstacles++;
+        }
+    }
+}
+
+export function generateStartStop(grid: Grid): void {
+    const walls = getEgdeCoordinates(grid)
+    const length = walls.length
+
+    const randomStartFieldIndex = Math.floor(Math.random() * length);
+    const startField = walls[randomStartFieldIndex];
+    let endField: Position;
+
+    do {
+        let randomIndex;
+        do {
+            randomIndex = Math.floor(Math.random() * length);
+        }
+        while (randomIndex === randomStartFieldIndex)
+
+        endField = walls[randomIndex]
+    }
+    while (isNextToOrTheSamePosition(grid, startField, endField))
+
+    addFieldGrid(grid, startField, "start");
+    addFieldGrid(grid, endField, "end");
+}
+
+export function printBoard(grid: Grid): void {
+    const formattedBoard = grid.map(row => 
+        row.map(cell => cell.padEnd(10, ' ')).join('')
+    ).join('\n');
+
+    console.log(formattedBoard);
+}
+
+export function findFirstOccurence(grid: Grid, type: Tile): Position | null {
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            if (grid[row][col] === type) {
+                return { x: col, y:row };
+            }
+        }
+    }
+    return null;
+}
+
+function main(): void {
+    const board = generateBoard(8, 10);
+    generateStartStop(board);
+    generateObstacles(board, 10);
+    printBoard(board);
+}
+
+main();
--- 03/tests/game.test.ts
+++ 03/tests/game.test.ts
@@ -1,41 +1,90 @@
-import { getEgdeCoordinates, generateBoard, Grid } from '../game';
+import { getEgdeCoordinates, left, right, up, down, generateBoard, addFieldGrid, Grid, generateObstacles, isNextToOrTheSamePosition, generateStartStop, findFirstOccurence, printBoard, Tile } from '../game';
 
-describe('generateBoard', () => {
+describe('Generate board - generateBoard', () => {
   test('should throw error on too small board', () => {
     expect(() => {
-      generateBoard(4, 5, 0);
+      generateBoard(4, 5);
+    }).toThrow();
+  });
+
+  test('should throw error when try to use negative numbers', () => {
+    expect(() => {
+      generateBoard(4, -5);
     }).toThrow();
   });
 
   test('should return board of minimal size', () => {
     const rows = 5;
     const cols = 5;
-    const board = generateBoard(cols, rows, 0);
+    const board = generateBoard(cols, rows);
 
     expect(board.length).toBe(rows);
     expect(board[0].length).toBe(cols);
   });
 
   test('should return board of size 5x10', () => {
-    const rows = 5;
-    const cols = 10;
-    const board = generateBoard(cols, rows, 0);
+    const rows = 10;
+    const cols = 5;
+    const board = generateBoard(cols, rows);
 
     expect(board.length).toBe(rows);
     expect(board[0].length).toBe(cols);
   });
 });
 
-describe('getEdgeCoordinates', () => {
+describe('Get egde (walls) coordinates - getEgdeCoordinates', () => {
   it("should return correct edge coordinates for a board", () => {
-    const board: Grid = generateBoard(5, 6, 0);
+    const board: Grid = generateBoard(5, 7);
 
     const expectedEdges = [
-      { x: 0, y: 0 }, { y: 0, x: 1 }, { y: 0, x: 2 }, { y: 0, x: 3 }, { y: 0, x: 4 }, // Top row
-      { y: 5, x: 0 }, { y: 5, x: 1 }, { y: 5, x: 2 }, { y: 5, x: 3 }, { y: 5, x: 4 }, // Bottom row
-      { y: 1, x: 0 }, { y: 2, x: 0 }, { y: 3, x: 0 }, { y: 4, x: 0 },                 // Left column
-      { y: 1, x: 4 }, { y: 2, x: 4 }, { y: 3, x: 4 }, { y: 4, x: 4 }                  // Right column
+      { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }, { x: 3, y: 0 }, { x: 4, y: 0 },
+      { x: 0, y: 1 },                                                 { x: 4, y: 1 },
+      { x: 0, y: 2 },                                                 { x: 4, y: 2 },
+      { x: 0, y: 3 },                                                 { x: 4, y: 3 },
+      { x: 0, y: 4 },                                                 { x: 4, y: 4 },
+      { x: 0, y: 5 },                                                 { x: 4, y: 5 },
+      { x: 0, y: 6 }, { x: 1, y: 6 }, { x: 2, y: 6 }, { x: 3, y: 6 }, { x: 4, y: 6 }
     ];
+
+    const edges = getEgdeCoordinates(board);
+
+    expectedEdges.forEach(egde => {
+      expect(edges).toContainEqual(egde);
+    });
+
+    expect(edges.length).toEqual(expectedEdges.length);
+  });
+
+  it("should return correct edge coordinates for a board 2", () => {
+    const board: Grid = generateBoard(7, 5);
+    
+    const expectedEdges = [
+      { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }, { x: 3, y: 0 }, { x: 4, y: 0 }, { x: 5, y: 0 }, { x: 6, y: 0 },
+      { x: 0, y: 1 },                                                                                 { x: 6, y: 1 },
+      { x: 0, y: 2 },                                                                                 { x: 6, y: 2 },
+      { x: 0, y: 3 },                                                                                 { x: 6, y: 3 },
+      { x: 0, y: 4 }, { x: 1, y: 4 }, { x: 2, y: 4 }, { x: 3, y: 4 }, { x: 4, y: 4 }, { x: 5, y: 4 }, { x: 6, y: 4 }
+    ]
+
+    const edges = getEgdeCoordinates(board);
+
+    expectedEdges.forEach(egde => {
+      expect(edges).toContainEqual(egde);
+    });
+
+    expect(edges.length).toEqual(expectedEdges.length);
+  });
+
+  it("should return correct edge coordinates for a board 3", () => {
+    const board: Grid = generateBoard(6, 5);
+    
+    const expectedEdges = [
+      { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }, { x: 3, y: 0 }, { x: 4, y: 0 }, { x: 5, y: 0 },
+      { x: 0, y: 1 },                                                                 { x: 5, y: 1 },
+      { x: 0, y: 2 },                                                                 { x: 5, y: 2 },
+      { x: 0, y: 3 },                                                                 { x: 5, y: 3 },
+      { x: 0, y: 4 }, { x: 1, y: 4 }, { x: 2, y: 4 }, { x: 3, y: 4 }, { x: 4, y: 4 }, { x: 5, y: 4 },
+    ]
 
     const edges = getEgdeCoordinates(board);
 
@@ -53,4 +102,205 @@ describe('getEdgeCoordinates', () => {
     expect(edges).toEqual([]);
   });
 
+});
+
+describe('Add field to board - addFieldGrid', () => {
+  it("should change value of field of board", () => {
+    const board: Grid = generateBoard(5, 6);
+    addFieldGrid(board, {x: 1, y: 0}, "wall")
+    expect(board[0][1]).toEqual("wall");
+  })
+
+  it("should change value of field on egde of board", () => {
+    const board: Grid = generateBoard(7, 6);
+    addFieldGrid(board, {x: 6, y: 5}, "wall")
+    expect(board[5][6]).toEqual("wall");
+  })
+
+  test('should throw error when try to add field outside size of board', () => {
+    const board: Grid = generateBoard(5, 6);
+    expect(() => {
+      addFieldGrid(board, {x: 1, y: 7}, "wall");
+    }).toThrow();
+  });
+
+  test('should throw error when try to use negative numbers', () => {
+    const board: Grid = generateBoard(5, 6);
+    expect(() => {
+      addFieldGrid(board, {x: 1, y: -1}, "wall");
+    }).toThrow();
+  });
+
+  test('should throw error when try to overwrite start/end field', () => {
+    const board: Grid = generateBoard(5, 6);
+    addFieldGrid(board, {x:0, y:0}, "start");
+    addFieldGrid(board, {x:0, y:3}, "end");
+    expect(() => {
+      addFieldGrid(board, {x:0, y:0}, "obstacle");
+    }).toThrow();
+    expect(() => {
+      addFieldGrid(board, {x:0, y:3}, "obstacle");
+    }).toThrow();
+  });
+});
+
+describe('Generate Obstacles fields - generateObstacles', () => {
+  it("should generate obstacles", () => {
+    const board: Grid = generateBoard(5, 6);
+    generateObstacles(board, 3)
+
+    let obstacleCount = 0;
+    for (const row of board) {
+      for (const tile of row) {
+        if (tile === "obstacle") {
+          obstacleCount++;
+        }
+      }
+    }
+    expect(obstacleCount).toEqual(3);
+  })
+  it("should throw when is to many obstacles", () => {
+    const board: Grid = generateBoard(5, 6);
+    expect(() => {
+      generateObstacles(board, 30)
+    }).toThrow();
+  })
+  it("should throw when is to many obstacles - upper treshold", () => {
+    const board: Grid = generateBoard(5, 6);
+    expect(() => {
+      generateObstacles(board, 8)
+    }).toThrow();
+  })
+  it("should generate obstacles - lower treshold", () => {
+    const board: Grid = generateBoard(5, 6);
+    generateObstacles(board, 7)
+
+    let obstacleCount = 0;
+    for (const row of board) {
+      for (const tile of row) {
+        if (tile === "obstacle") {
+          obstacleCount++;
+        }
+      }
+    }
+    expect(obstacleCount).toEqual(7);
+  })
+})
+
+describe('Check if those fields are next to each other - isNextToOrTheSamePosition', () => {
+  let board: Grid;
+  beforeEach(() => {
+    board = generateBoard(6, 5)
+  }); 
+  it("should check the same position as true", () => {
+    expect(isNextToOrTheSamePosition(board, {x: 0, y:0}, {x: 0, y:0})).toEqual(true)
+  })
+
+  it("should check the side by side position as true", () => {
+    expect(isNextToOrTheSamePosition(board, { x: 1, y: 0 }, { x: 2, y: 0 })).toEqual(true)
+  })
+
+  it("should check the side by side position as true 2", () => {
+    expect(isNextToOrTheSamePosition(board, {x: 0, y:0}, {x: 0, y:1})).toEqual(true)
+  })
+
+  it("should check the one field separation position as false", () => {
+    expect(isNextToOrTheSamePosition(board, {x: 0, y:0}, {x: 0, y:2})).toEqual(false)
+  })
+
+  test('should throw error on positions outside board', () => {
+    expect(() => {
+      isNextToOrTheSamePosition(board, {x: 0, y:4}, {x: 0, y:8});
+    }).toThrow();
+  });
+});
+
+describe('Generate Start and End fields - generateStartStop', () => {
+  let board: Grid;
+  beforeEach(() => {
+    board = generateBoard(5, 7)
+  }); 
+  it("should generate start and stop", () => {
+    generateStartStop(board)
+
+    let startCount = 0;
+    for (const row of board) {
+      for (const tile of row) {
+        if (tile === "start") {
+          startCount++;
+        }
+      }
+    }
+    let endCount = 0;
+    for (const row of board) {
+      for (const tile of row) {
+        if (tile === "end") {
+          endCount++;
+        }
+      }
+    }
+
+    expect(endCount).toEqual(1);
+    expect(startCount).toEqual(1);
+  })
+
+  it("should generate end on edge", () => {
+    generateStartStop(board)
+    const edges = getEgdeCoordinates(board);
+    const pos = findFirstOccurence(board, 'end')
+
+    expect(edges).toContainEqual(pos);
+  })
+
+  it("should generate start on edge", () => {
+    generateStartStop(board)
+    const edges = getEgdeCoordinates(board);
+    const pos = findFirstOccurence(board, 'start')
+
+    expect(edges).toContainEqual(pos);
+  })
+});
+
+describe('Movement - left, right, up, down', () => {
+  let board: Grid;
+  beforeEach(() => {
+    board = [
+      ['wall',      'start', 'obstacle', 'wall',  'wall',   'wall',   'wall'],
+      ['obstacle',  'empty', 'obstacle', 'empty', 'empty',  'empty',  'wall'],
+      ['obstacle',  'empty', 'obstacle', 'empty', 'empty',  'empty',  'wall'],
+      ['wall',      'empty', 'empty',    'empty', 'empty',  'empty',  'wall'],
+      ['wall',      'wall',   'wall',    'wall',  'end',    'wall',   'wall'],
+    ]
+  }); 
+  it("should make move left from start", () => {
+    expect(left(board, {x: 1, y: 0})).toEqual({x: 0, y: 0});
+  })
+  it("should not make move left into obstacle", () => {
+    expect(left(board, {x: 1, y: 1})).toEqual(false);
+  })
+  it("should not make move left out of the board", () => {
+    expect(left(board, {x: 0, y: 0})).toEqual(false);
+  })
+  it("should let make move into end", () => {
+    expect(left(board, {x: 5, y: 4})).toEqual(true);
+  })
+
+  it("should not make move right from start", () => {
+    expect(right(board, {x: 1, y: 0})).toEqual(false);
+  })
+  it("should not make move right into obstacle", () => {
+    expect(left(board, {x: 1, y: 1})).toEqual(false);
+  })
+  it("should not make move right out of the board", () => {
+    expect(right(board, {x: 6, y: 4})).toEqual(false);
+  })
+  it("should let make move into end", () => {
+    expect(right(board, {x: 3, y: 4})).toEqual(true);
+  })
+  it("should let make move up", () => {
+    expect(up(board, {x: 1, y: 2})).toEqual({x: 1, y: 1});
+  })
+  it("should let make move down", () => {
+    expect(down(board, {x: 1, y: 2})).toEqual({x: 1, y: 3});
+  })
 });

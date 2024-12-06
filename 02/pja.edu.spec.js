ab6187dbb0811c42b43e92ad9ad0c9c5c5c0b855
Rafał Sojecki <s26286@pjwstk.edu.pl>
2024-12-06 23:27:01 +0100

    set safari window size, finish 8 tests

--- 02/pja.edu.spec.js
+++ 02/pja.edu.spec.js
@@ -7,6 +7,7 @@ describe('pja.edu.pl', function () {
 
     before(async function () {
         driver = new Builder().forBrowser(Browser.SAFARI).build();
+        await driver.manage().window().setRect({ width: 1280, height: 720 });
     });
 
     after(async () => await driver.quit());
@@ -28,25 +29,45 @@ describe('pja.edu.pl', function () {
         assert.ok(!await banner.isDisplayed(), "Cookie banner should be not displayed");
     });
 
-    // it('should navigate to the about page', async function () {
-    //     await driver.get(page);
-    //     driver.manage().addCookie("cookies_setup", "W3RydWUsdHJ1ZSx0cnVlXQ%3D%3D");
-
-    //     const aboutLink = await driver.findElement(By.css("a[href='https://pja.edu.pl/o-uczelni/']"));
-    //     const linkt = await aboutLink.getHref()
-    //     console.log(linkt)
-    //     await aboutLink.click();
-    //     // await new Promise(resolve => setTimeout(resolve, 2000));
-    //     const heading = await driver.findElement(By.css("h2"));
-    //     const headingText = await heading.getText();
-    //     console.log(headingText)
-    //     assert.ok(headingText.includes("O Uczelni"), "About page should load with the correct heading");
-    // });
-
     it('have a header', async function () {
         await driver.get(page);
         const header = await driver.findElement(By.className("top-nav"));
         assert.ok(await header.isDisplayed(), "Header should be displayed");
+    });
+
+    it('should navigate to the about page from header', async function () {
+        driver.manage().addCookie("cookies_setup", "W3RydWUsdHJ1ZSx0cnVlXQ%3D%3D");
+        await driver.get(page);
+        
+        const aboutLink = await driver.findElement(By.css("#menu-top-left-menu > li > a[href='https://pja.edu.pl/o-uczelni/']"));
+
+        await aboutLink.click();
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        const heading = await driver.findElement(By.css("h2"));
+        const headingText = await heading.getText();
+        assert.ok(headingText.includes("Uczelni"), "About page should load with the correct heading"); //&nbsp; w nazwie
+    });
+
+    it('have svg logo', async function () {
+        await driver.get(page);
+        const logo = await driver.findElement(By.css(".side-nav__logo svg"));
+        assert.ok(await logo.isDisplayed(), "Logo should be displayed");
+    });
+
+    it('search functionality in side nav works', async function () {
+        driver.manage().addCookie("cookies_setup", "W3RydWUsdHJ1ZSx0cnVlXQ%3D%3D");
+        await driver.get(page);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const searchLink = await driver.findElement(By.css(".side-nav .js-search"));
+
+        await searchLink.click();
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const searchInput = await driver.findElement(By.css(".full-search__input"));
+        await searchInput.sendKeys("Selenium");
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const searchResults = await driver.findElement(By.css(".full-search__results"));
+
+        assert.ok(searchResults.isDisplayed(), "Result banner should be visible after putting query");
     });
 
     it('have footer', async function () {
